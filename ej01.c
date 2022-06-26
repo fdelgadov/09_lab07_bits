25f6b93b53d2d7d6e741d515dad5cf16409a0c3f
fdelgadov <fdelgadov@unsa.edu.pe>
2022-06-26 16:07:15 -0500

    bis(): La conversión a cadena de bits se hace en otra funcion
    - int2bin(): Convierte un entero en una cadena de caracteres que representan
      el mismo número en binario.

--- ej01.c
+++ ej01.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char* int2bin(int n){
+  char* res = malloc(sizeof(char) * 33);
+  for(int i = 0; i < 32; i++)
+    res[i] = '0';
+  res[32] = 0;
+
+  for(int i = 0; i < 32 && n > 0; i++){
+    if(n % 2 == 0)
+      res[31 - i] = '0';
+    else
+      res[31 - i] = '1';
+    n = n / 2;
+  }
+
+  return res;
+}
 
 int bis(int x, int m){
-  char ax[] = "00000000000000000000000000000000";
-  char am[] = "00000000000000000000000000000000";
-
-  for(int i = 0; i < 32 && x > 0; i++){
-    if(x % 2 == 0)
-      ax[31 - i] = '0';
-    else
-      ax[31 - i] = '1';
-    x = x / 2;
-  }
-
-  for(int i = 0; i < 32 && m > 0; i++){
-    if(m % 2 == 0)
-      am[31 - i] = '0';
-    else
-      am[31 - i] = '1';
-    m = m / 2;
-  }
-  printf("%s\n", ax);
-  printf("%s\n", am);
+  char* ax = int2bin(x);
+  char* am = int2bin(m);
 
   for(int i = 0; i < 32; i++){
     if(am[i] == '1')
@@ -34,6 +34,10 @@ int bis(int x, int m){
   }
 
   printf("%s\n%d\n", ax, res);
+  free(ax);
+  free(am);
+  
+  return res;
 }
 
 void main(){
